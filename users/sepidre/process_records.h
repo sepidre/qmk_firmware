45142eddb5eba6500a59a1574786afe8836acee0
Stephan <github@athome.zone>
2024-02-20 15:51:13 +0100

    some changes

--- users/sepidre/process_records.h
+++ users/sepidre/process_records.h
@@ -68,11 +68,5 @@ enum userspace_custom_keycodes {
 bool process_record_secrets(uint16_t keycode, keyrecord_t *record);
 bool process_record_keymap(uint16_t keycode, keyrecord_t *record);
 
-#define KC_SEC1 KC_SECRET_1
-#define KC_SEC2 KC_SECRET_2
-#define KC_SEC3 KC_SECRET_3
-#define KC_SEC4 KC_SECRET_4
-#define KC_SEC5 KC_SECRET_5
-
 #define KC_RESET RESET
 #define KC_RST   KC_RESET
